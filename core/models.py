1ed3dfec3ada0ebd863f13c9fb5f44c126a03304
Chong Kim <chong@gridium.com>
2020-01-24 15:37:24 -0600

    add ProductListing model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class ProductListing(models.Model):
+    name = models.TextField(max_length=255)
+    price = models.DecimalField(decimal_places=2, max_digits=20)
+    slug = models.TextField(max_length=255)
+    uuid = models.TextField(max_length=255)
